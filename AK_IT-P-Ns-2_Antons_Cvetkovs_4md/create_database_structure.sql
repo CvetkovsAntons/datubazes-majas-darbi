b151efa06e46e141f4cee406335860fe91adf777
Antons Cvetkovs <antonscvetkovs12@gmail.com>
2024-01-07 21:14:21 +0200

    4md

--- AK_IT-P-Ns-2_Antons_Cvetkovs_4md/create_database_structure.sql
+++ AK_IT-P-Ns-2_Antons_Cvetkovs_4md/create_database_structure.sql
@@ -3,7 +3,7 @@ CREATE DATABASE sport_club;
 CREATE TABLE sport_club.dbo.client (
     personal_code VARCHAR(13)
         CHECK (
-            LEN(personal_code) = 13 AND
+            LEN(personal_code) = 12 AND
             ISNUMERIC(SUBSTRING(personal_code, 1, 6)) = 1 AND
             SUBSTRING(personal_code, 7, 1) = '-' AND
             ISNUMERIC(SUBSTRING(personal_code, 8, 5)) = 1
@@ -20,31 +20,39 @@ CREATE TABLE sport_club.dbo.client (
 );
 
 CREATE TABLE sport_club.dbo.gym (
-    id INT,
-    address VARCHAR(100),
-    CONSTRAINT gym_pk PRIMARY KEY (id)
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    country VARCHAR(100),
+    city VARCHAR(100),
+    street VARCHAR(100),
+    building VARCHAR(100),
 );
 
 CREATE TABLE sport_club.dbo.sport (
-    id INT,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     name VARCHAR(100),
-    CONSTRAINT sport_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE sport_club.dbo.gym_sport (
     sport_id INT,
     gym_id INT,
     CONSTRAINT gym_sport_pk PRIMARY KEY (sport_id, gym_id),
-    CONSTRAINT gym_sport_sport_id_fk FOREIGN KEY (sport_id) REFERENCES sport_club.dbo.sport (id),
+    CONSTRAINT gym_sport_sport_id_fk FOREIGN KEY (sport_id) REFERENCES sport_club.dbo.sport (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT gym_sport_gym_id_fk FOREIGN KEY (gym_id) REFERENCES sport_club.dbo.gym (id)
-);
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
 
 CREATE TABLE sport_club.dbo.session (
-    id INT,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     client VARCHAR(13),
     gym_id INT,
     date DATETIME NOT NULL DEFAULT GETDATE(),
-    CONSTRAINT session_id PRIMARY KEY (id),
-    CONSTRAINT session_client_fk FOREIGN KEY (client) REFERENCES sport_club.dbo.client (personal_code),
+    CONSTRAINT session_client_fk FOREIGN KEY (client) REFERENCES sport_club.dbo.client (personal_code)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT session_gym_id_fk FOREIGN KEY (gym_id) REFERENCES sport_club.dbo.gym (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
